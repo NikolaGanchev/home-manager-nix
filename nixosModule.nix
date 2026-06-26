af2a991c15bfb940104adee3eb3328b0e7df8bc2
Nikola Ganchev <nikolamihailovg@gmail.com>
2026-06-26 22:04:23 +0300

    Improve profile support

--- nixosModule.nix
+++ nixosModule.nix
@@ -1,24 +1,36 @@
 { config, lib, pkgs, ...}: {
     options.home-manager2 = {
-        profiles = lib.mkOption {
-            type = lib.types.attrsOf (lib.types.submodule ({config, ...}: {
+        users = lib.mkOption {
+            type = lib.types.attrsOf (lib.types.submodule ({name, config, ...}: {
                 options = {
-                    config = lib.mkOption {
+                    profiles = lib.mkOption {
                         type = lib.types.attrsOf lib.types.attrs;
                         default = {};
                     };
 
-                    enable = lib.mkOption {
-                        type = lib.types.bool;
-                    };
-
-                    username = lib.mkOption {
-                        type = lib.types.str;
-                    };
-
                     home = lib.mkOption {
                         type = lib.types.path;
-                        default = "/home/${config.user}";
+                        default = "/home/${name}";
+                    };
+
+                    activeProfile = lib.mkOption {
+                        type = lib.types.nullOr lib.types.str;
+                        default = 
+                            let 
+                                profileNames = builtins.attrNames config.profiles;
+                                profileCount = builtins.length profileNames;
+                            in
+                                if profileCount == 1 then
+                                    builtins.trace ''
+                                        home-manager2: No explicit 'activeProfile' for user '${name}'.
+                                        Single profile found and selected: '${builtins.head profileNames}' ''
+                                    builtins.head profileNames
+                                else if profileCount > 1 then
+                                    builtins.throw ''
+                                        home-manager2: Ambiguous configuration for user '${name}'.
+                                        Found ${toString profileCount} profiles: ${lib.concatStringsSep ", " profileNames}.
+                                        '${name}.activeProfile' must be explicitly set.''
+                                else null;
                     };
                 };
             }));
@@ -36,18 +48,23 @@
         home-manager2.extraModules = [ ./modules/git.nix ];
 
         system.activationScripts.home-manager2-activate = lib.concatStringsSep "\n" (
-            lib.mapAttrsToList (profile: profileConfig: 
-                let pkg = import ./lib/evaluateConfig.nix { 
-                    inherit pkgs lib; 
-                    configuration = profileConfig.config; 
-                    genericModule = ./modules/generic.nix; 
+            lib.mapAttrsToList (user: userConfig: 
+                let 
+                    pkg = import ./lib/evaluateConfig.nix { 
+                        inherit pkgs lib; 
+                        configuration = userConfig.profiles."${userConfig.activeProfile}"; 
+                        genericModule = ./modules/generic.nix; 
 
-                    extraModules = config.home-manager2.extraModules;
-                }; 
-                in ''
-                    echo "Activating home-manager2 for user ${profileConfig.username}"
-                    runuser -u ${profileConfig.username} -- env HOME="${config.users.users.${profileConfig.username}.home}" USER="${profileConfig.username}" "${pkg}/commit"
-                '') (lib.attrsets.filterAttrs (name: value: value.enable) config.home-manager2.profiles)
+                        extraModules = config.home-manager2.extraModules;
+                    }; 
+                in 
+                    if !(builtins.hasAttr userConfig.activeProfile userConfig.profiles) then
+                        builtins.throw "home-manager2: Specified 'activeProfile' '${userConfig.activeProfile}' for user '${user}' does not exist."
+                    else
+                    ''
+                        echo "home-manager2: Activating profile '${userConfig.activeProfile}' for user ${user}"
+                        runuser -u ${user} -- env HOME="${userConfig.home}" USER="${user}" "${pkg}/commit"
+                    '') (lib.attrsets.filterAttrs (name: value: value.activeProfile != null) config.home-manager2.users)
         );
     };
 }
